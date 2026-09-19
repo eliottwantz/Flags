34f3b0c7ad2934f1f3b36a1c7ef9762112bdedcf
Eliott Wantz <eliott.wantz@develiott.com>
2026-09-19 11:17:44 -0400

    Initial Commit

---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,18 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Eliott Wantz on 19/09/2026.
+//  SPDX-License-Identifier: MIT
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
